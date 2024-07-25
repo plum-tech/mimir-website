d59b05f03b7033d30a3479f61ab58cc473f8d4b9
Liplum <Li_plum@outlook.com>
2024-07-26 02:45:24 +0800

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./footer";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body className={inter.className}>
+        <Header />
         {children}
         <Footer />
       </body>
