2209e20206bf1b27c1db7aa25995d8e4e9358d66
Liplum <Li_plum@outlook.com>
2024-08-12 10:25:27 +0800

    small fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -31,7 +31,7 @@ const FooterSectionComp = ({
   items?: FooterSectionItem[]
 }) => {
   return <div>
-    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:`text-white`">{label}</h2>
+    <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{label}</h2>
     <ul className="text-gray-400 font-medium">
       {items && items?.map(item => (
         <FooterSectionItemComp
