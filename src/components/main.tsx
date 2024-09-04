1c36f27a3afd4cecf73e55e9e89e8635932a3f09
Liplum <Li_plum@outlook.com>
2024-09-05 05:06:54 +0800

    todo

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react"
 export default function MainFramework({ children }: {
   children: ReactNode
 }) {
+  // TODO: Remove "flex flex-col" for desktop support
   return <main className="flex flex-col min-h-screen items-center justify-nomarl lg:p-24">
     {children}
   </main>
