884707a1c1cc488b367f67b65d1625bd2686dde0
Liplum <Li_plum@outlook.com>
2024-09-09 04:52:12 +0800

    completed privacy policy

---
+++ src/app/privacy-policy/[version]/page.tsx
@@ -0,0 +1,30 @@
+import MainFramework from "@/components/main"
+import Title from "@/components/title"
+import dynamic from 'next/dynamic'
+import { redirect } from "next/navigation"
+
+const versions = [
+  "20240915"
+]
+
+export default async function Page({
+  params
+}: {
+  params: { version: string }
+}) {
+  const { version } = params
+  if (!versions.includes(version)) {
+    return redirect("/tos/20240915")
+  }
+  const DynamicPriacyPolicy = dynamic(() => import(`../${version}.md`))
+
+  return <MainFramework>
+    <Title
+      title="隐私政策"
+      desc={`${version}版`}
+    />
+    <div className="px-4 md:px-8 lg:px-16">
+      <DynamicPriacyPolicy />
+    </div>
+  </MainFramework>
+}
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,15 +1,5 @@
-import MainFramework from "@/components/main";
-import Title from "@/components/title";
-import PrivacyPolicy from "./zh-Hans.md"
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return <MainFramework>
-    <Title
-      title="隐私政策"
-      desc=""
-    />
-    <div className="px-4 md:px-8 lg:px-16">
-      <PrivacyPolicy />
-    </div>
-  </MainFramework>
+  return redirect("/privacy-policy/20240915")
 }
