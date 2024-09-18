768d74cb3c75750d08f5979b73eb3b02c20e12b2
Aregene <sh1212121245@126.com>
2024-09-18 18:26:15 +0800

    增加邮箱链接和url链接

--- src/app/about/components.tsx
+++ src/app/about/components.tsx
@@ -1,15 +1,35 @@
 import { Card } from "@/components/card"
 import { ContributorInfo } from "./list"
+import { ContributorContact } from "./list"
 
-export const ContributorCard = ({
-  info
-}: {
-  info: ContributorInfo
-}) => {
-  return <Card header={info.name}>
-    {info.contact.map((contact, index) => <div key={index}>
-      {contact.type}: {contact.content}
-    </div>)}
-    {info.description}
-  </Card>
-}
+
+const GreetContributor = ({ contact }: { contact: ContributorContact }) => {
+  switch (contact.type) {
+    case 'phone':
+      return <div>phone : {contact.content}</div>;
+    case 'email':
+      return <div>
+      email : 
+      <a href={`mailto:}${contact.content}`} className="link">{contact.content}</a>
+      </div>;
+    case 'qq':
+      return <div>qq : {contact.content}</div>;
+    case 'wechat':
+      return <div>wechat : {contact.content}</div>;
+    case 'url':
+      return <a href={contact.content} className="block">url : {contact.content}</a>;;
+
+  }
+};
+
+export const ContributorCard = ({ info }: { info: ContributorInfo }) => {
+  return (
+    <Card header={info.name}>
+      {info.contact.map((contact, index) => (
+        <GreetContributor key={index} contact={contact} />
+      ))}
+      {info.description}
+    </Card>
+  );
+};
+
