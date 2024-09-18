7f2eeedbb9388576dd6e0aa1f2b04be93e89109a
Aregene <sh1212121245@126.com>
2024-09-18 21:50:49 +0800

    活跃or历史

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,19 +3,31 @@ import Title from "@/components/title";
 import contributors from "./contributors.json";
 import { ContributorCard } from "./components";
 import { ContributorInfo } from "./list";
-
+const activeContributors = contributors.filter(info => !info.endDate);
+const historicalContributors = contributors.filter(info => info.endDate);
 export default function Page() {
   return <MainFramework>
     <Title
       title="关于我们"
       desc="上海应用技术大学小应生活团队"
     />
+    <p className="block w-96 p-3 bg-green-500 text-white text-center rounded-lg shadow-md">活跃成员</p>
+    <br/>
     <div className="py-2 grid lg:grid-cols-2 xl:grid-cols-3 gap-4">
-      {
-        contributors.map((info, index) => <ContributorCard
-          key={index} info={info as ContributorInfo}
-        />)
-      }
-    </div>
+        {/* 渲染活跃用户 */}
+        
+        {activeContributors.map((info, index) => (
+          <ContributorCard key={index} info={info as ContributorInfo} />
+        ))}
+      </div>
+      <br/>
+      <p className="block w-96 p-3 bg-gray-500 text-white text-center rounded-lg shadow-md">历史成员</p>
+      <br/>
+      <div className="py-2 grid lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        {/* 渲染历史用户 */}
+        {historicalContributors.map((info, index) => (
+          <ContributorCard key={index} info={info as ContributorInfo} />
+        ))}
+      </div>
   </MainFramework>
 }
