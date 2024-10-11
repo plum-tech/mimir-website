8a4c5b113e3216dc80008789fc37f984f8a9709e
Liplum <Li_plum@outlook.com>
2024-10-11 11:16:33 +0800

    added icon in /public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <head>
         <meta itemProp="name" content="小应生活" />
         <meta itemProp="description" content="小应生活 - 你的校园生活，从未如此简单。上应人必备App，上海应用技术大学小应生活团队开发" />
-        <meta itemProp="image" content="https://www.xiaoying.life/icon-512x.png" />
+        <meta itemProp="image" content="https://www.xiaoying.life/icon-256x.png" />
         <meta name="baidu-site-verification" content="codeva-l41HNYleld" />
       </head>
       <body className={inter.className}>
