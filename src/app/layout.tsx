b676736099e67a6bf24a6c507edc82a36fbef895
Liplum <Li_plum@outlook.com>
2024-11-24 16:25:38 +0800

    filled icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
       <head>
         <meta itemProp="name" content="小应生活" />
         <meta itemProp="description" content="小应生活 - 校园生活就用小应生活。上应人必备App，上海应用技术大学小应生活团队开发" />
-        <meta itemProp="image" content="https://www.xiaoying.life/icon-256x.png" />
-        <meta name="baidu-site-verification" content="codeva-l41HNYleld" />
+        <meta itemProp="image" content="https://www.xiaoying.life/icon-filled-256x.png" />
+        <meta name="baidu-site-verification" content="codeva-btW1cVqDcz" />
       </head>
       <body className={`${inter.className}`}>
         {children}
